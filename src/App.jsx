7ec8e15d0e76ba12ef8c0209743ecd044a4593c0
ravgusha <ravgusha@gmail.com>
2023-05-19 20:25:44 +0300

    feat: add basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Button, Text, Container } from "@nextui-org/react";
 
 import "./App.css";
 
-const baseURL = "https://v2.jokeapi.dev/joke/Any?";
+const baseURL = "https://v2.jokeapi.dev/joke/Any?type=single";
 
 function App() {
   const [joke, setJoke] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      console.log(response.data.joke);
-      setJoke(response.data.joke);
-    });
+    axios
+      .get(baseURL)
+      .then((response) => {
+        console.log(response);
+        setJoke(response.data.joke);
+      })
+      .catch((error) => {
+        setError(error);
+      });
   }, []);
 
-  if (!joke) return null;
+  if (error) return `Error: ${error}`;
+  // if (!joke) return null;
 
   return (
-    <div>
-      <p>{joke}</p>
-    </div>
+    <Container display="flex" justify="center" alignItems="center">
+      <Text h3 color="#19A7CE" css={{ flexBasis: "100%" }}>
+        {joke}
+      </Text>
+      <Button css={{ background: "#146C94" }}>Click me</Button>
+    </Container>
   );
 }
 
