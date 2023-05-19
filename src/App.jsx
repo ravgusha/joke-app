ae2578a268c75b8f77b2460c0f779af9e15fe333
ravgusha <ravgusha@gmail.com>
2023-05-19 21:04:21 +0300

    refactor: refactor error displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Button, Text, Container } from "@nextui-org/react";
+import { Button, Text, Container} from "@nextui-org/react";
+
+import logo from "./logo.svg";
 
 import "./App.css";
 
@@ -11,6 +13,10 @@ function App() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    getJoke();
+  }, []);
+
+  const getJoke = () => {
     axios
       .get(baseURL)
       .then((response) => {
@@ -20,17 +26,31 @@ function App() {
       .catch((error) => {
         setError(error);
       });
-  }, []);
-
-  if (error) return `Error: ${error}`;
-  // if (!joke) return null;
+  };
 
   return (
     <Container display="flex" justify="center" alignItems="center">
-      <Text h3 color="#19A7CE" css={{ flexBasis: "100%" }}>
-        {joke}
-      </Text>
-      <Button css={{ background: "#146C94" }}>Click me</Button>
+      <img src={logo} className="logo" alt="logo" />
+      {error ? (
+        <Text
+          h4
+          color="error"
+          css={{ flexBasis: "100%", padding: "0 10% 30px", textAlign: "center" }}
+        >
+          {error}
+        </Text>
+      ) : (
+        <Text
+          h3
+          color="#19A7CE"
+          css={{ flexBasis: "100%", padding: "0 10% 30px", textAlign: "center" }}
+        >
+          {joke}
+        </Text>
+      )}
+      <Button size="xl" css={{ background: "#146C94" }} onClick={getJoke}>
+        Get New Joke
+      </Button>
     </Container>
   );
 }
