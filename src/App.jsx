36a5a884213b71282faf127409bafae50c5918d8
ravgusha <ravgusha@gmail.com>
2023-05-19 19:31:26 +0300

    feat: add basic request

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+const baseURL = "https://v2.jokeapi.dev/joke/Any?";
 
 function App() {
+  const [joke, setJoke] = useState(null);
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      console.log(response.data.joke);
+      setJoke(response.data.joke);
+    });
+  }, []);
+
+  if (!joke) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{joke}</p>
     </div>
   );
 }
